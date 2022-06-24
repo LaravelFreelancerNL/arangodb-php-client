0457b0e28c601d79e1c2fb024af811bf8d610fce
Bas <info@laravel-freelancer.nl>
2022-06-24 15:12:41 +0200

    CI improvements

--- bin/qa.sh
+++ bin/qa.sh
@@ -1,18 +1,9 @@
 #!/usr/bin/env bash
-echo "Fix coding style"
-./vendor/bin/phpcbf
-
-echo "Check for remaining coding style errors"
-./vendor/bin/phpcs
-
-echo "Run PHPMD"
+printf "\nRun PHPMD\n"
 ./vendor/bin/phpmd src/ text phpmd-ruleset.xml
 
-echo "Run PHPStan"
-./vendor/bin/phpstan analyse -c phpstan.neon
+printf "\nRun PHPCPD\n"
+./vendor/bin/phpcpd src
 
-echo "Run Psalm"
-./vendor/bin/psalm  --no-cache
-
-echo "Run PHPUnit"
-./vendor/bin/phpunit
+printf "\nRun PHPStan\n"
+composer analyse
